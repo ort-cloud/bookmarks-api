182762b6f48dc3c10eb7ebb8b838ddb4e0e11689
Carl Ortutay <carlorty@gmail.com>
2019-10-15 21:58:15 -0400

    request still unauthorized

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  API_TOKEN: process.env.API_TOKEN || "1f690ad0-9d9c-498c-8f81-35d1ba4c41e1",
 };
